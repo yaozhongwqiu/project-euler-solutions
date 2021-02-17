86f57c0d0911ffbc677412712737c305a5bfb291
Yaozhong Qiu <yaozhong.wq@gmail.com>
2021-02-17 22:53:38 +1100

    added sol-2 to prob-1 and added benchmarks
    In the future, all subsequent solutions should have benchmarks.

--- problem-1.py
+++ problem-1.py
@@ -1,7 +1,20 @@
-# problem 1
+#problem 1
 
 def solution_1 () :
     multiples_of_3 = list(range(0, 1000, 3))
     multiples_of_5 = list(range(0, 1000, 5))
     x = list(set(multiples_of_3 + multiples_of_5))
     return(sum(x))
+
+def solution_2 () : 
+    multiples_of_3 = [x for x in range(1000) if x % 3 == 0]
+    multiples_of_5 = [x for x in range(1000) if x % 5 == 0]
+    x = list(set(multiples_of_3 + multiples_of_5))
+    return(sum(x)) 
+
+#benchmarks    
+%timeit solution_1()
+%timeit solution_2()
+
+#27.6 µs ± 426 ns per loop (mean ± std. dev. of 7 runs, 10000 loops each)
+#146 µs ± 1.01 µs per loop (mean ± std. dev. of 7 runs, 10000 loops each)
